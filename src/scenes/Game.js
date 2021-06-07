c25300330663e28e9d490509d254f1089e411d53
SantosMaycon <mayconalves.santos14@gmail.com>
2021-06-07 18:19:11 -0300

    add multiple platforms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -13,6 +13,21 @@ export default class Game extends Phaser.Scene {
   create() {
     // Adicionando a imagem no canvas/tela
     this.add.image(240, 320, "background");
-    this.physics.add.image(240, 320, "platform").setScale(0.5);
+
+    // Adicionando um grupo de elementos
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
+
+      /** @type {Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body;
+      body.updateFromGameObject();
+    }
   }
 }
